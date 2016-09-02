4e677421a7327a8eaed286115576b718e708e334
Jamie Godel <jrogodel@gmail.com>
2016-09-02 16:13:50 -0400

    Relocate the Heroes concerns into it's own file, refactor AppComponent into an app shell that only handles navigation.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }      from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:  [ 
@@ -12,11 +15,11 @@ import { HeroDetailComponent } from './hero-detail.component';
   ],
   declarations: [ 
     AppComponent,
-    HeroDetailComponent 
+    HeroesComponent, 
+    HeroDetailComponent
   ],
-  bootstrap:  [ 
-    AppComponent 
-  ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 
 export class AppModule { }
